63d1a7d166899bc0e7fa5b14029ed71c37541f5a
Ali Youssef <aliyoussef34@hotmail.fr>
2022-07-12 09:21:44 +0200

    fixed parsing

--- src/App.js
+++ src/App.js
@@ -1,41 +1,20 @@
 import React, { useEffect, useState } from "react";
 import { turnSoftwareDataToFormattedCSV } from "./lib/helpers/DataReportHelper";
+import Papa from "papaparse";
 const App = () => {
-  const [file, setFile] = useState();
   const [array, setArray] = useState([]);
 
-  const fileReader = new FileReader();
-
-  const handleOnChange = (e) => {
-    setFile(e.target.files[0]);
-  };
-
-  const csvFileToArray = (string) => {
-    const csvHeader = string.slice(0, string.indexOf("\n")).split(",");
-    const csvRows = string.slice(string.indexOf("\n") + 1).split("\n");
-
-    const array = csvRows.map((i) => {
-      const values = i.split(",");
-      const obj = csvHeader.reduce((object, header, index) => {
-        object[header] = values[index];
-        return object;
-      }, {});
-      return obj;
+  const parseFile = (file) => {
+    Papa.parse(file, {
+      header: true,
+      complete: (results) => {
+        setArray(results.data);
+      },
     });
-    setArray(array);
   };
 
   const handleOnSubmit = (e) => {
-    e.preventDefault();
-
-    if (file) {
-      fileReader.onload = function (event) {
-        const text = event.target.result;
-        csvFileToArray(text);
-      };
-
-      fileReader.readAsText(file);
-    }
+    parseFile(e.target.files[0]);
   };
 
   useEffect(() => {
@@ -52,16 +31,8 @@ const App = () => {
           type={"file"}
           id={"csvFileInput"}
           accept={".csv"}
-          onChange={handleOnChange}
+          onChange={handleOnSubmit}
         />
-
-        <button
-          onClick={(e) => {
-            handleOnSubmit(e);
-          }}
-        >
-          IMPORT CSV
-        </button>
       </form>
 
       <br />
